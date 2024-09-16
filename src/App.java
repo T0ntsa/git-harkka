0a11dc58234f9036ab97bfeb9eea7573ed21aa12
Toni Näsman <toni.nasman@student.hamk.fi>
2024-09-16 18:55:06 +0300

    First commit
    First java project commit. GitHub Desktop training.

---
+++ src/App.java
@@ -0,0 +1,32 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // Java harjoittelu alkaa
+        
+        System.out.println("Eka ohjelma hamkissa");
+
+        String nimi = "Toni Näsman";
+
+        System.err.println("Nimi on " + nimi + " ja olen Hämeenlinnassa");
+
+        int luku1 = 5;
+        int luku2 = 10;
+        int tulos;
+
+        luku1 = 2;
+        tulos = luku1 + luku2;
+
+        System.err.println("Tulos on " + tulos);
+
+
+
+
+        System.out.println("H");
+        System.out.println("A");
+        System.out.println("M");
+        System.out.println("K");
+
+
+
+    }
+}
